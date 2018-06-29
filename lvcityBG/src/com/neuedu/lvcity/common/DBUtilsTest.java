2af4cc26fa516ee7bf3af2523619442ce4bed9f4
qaz974613 <271686329@qq.com>
2018-06-29 14:55:57 +0800

    第一次推送

---
+++ lvcityBG/src/com/neuedu/lvcity/common/DBUtilsTest.java
@@ -0,0 +1,17 @@
+package com.neuedu.lvcity.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBUtilsTest {
+
+
+	@Test
+	public void testGetConnection() {
+		System.out.println(DBUtils.getConnection());
+	}
+		
+
+}
---
+++ lvcityBG/src/com/neuedu/lvcity/common/FileUtils.java
@@ -0,0 +1,8 @@
+package com.neuedu.lvcity.common;
+
+
+public class FileUtils {
+
+	//图片服务器地址
+	public static String UPLOAD_PATH = "C:\\Users\\mi\\Desktop\\javaweb\\apache-tomcat-7.0.88-windows-x64\\apache-tomcat-7.0.88\\webapps\\uploads";
+}
